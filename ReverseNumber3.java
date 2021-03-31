488b4020613ab2d1cd13195c2ecd043dc9a1159d
gourab98 <gourabsahag@gmail.com>
2021-03-31 13:06:47 +0600

    3rd problem submitted

---
+++ ReverseNumber3.java
@@ -0,0 +1,20 @@
+public class ReverseNumber3 {
+    
+    public static void main(String [] args){
+     
+        for(int i=1000;i>0;)
+        {
+            System.out.print(i+"\t");
+            i=i-1;
+            System.out.print(i+"\t");
+            i=i-1;
+            System.out.print(i+"\t");
+            i=i-1;
+            System.out.print(i+"\t");
+            i=i-1;
+            System.out.println(i+"\t");
+            i=i-1;
+
+        }
+    }
+}
