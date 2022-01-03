482322e245af4e0aa3415f425bb022824fd31f03
gourab98 <gourabsahag@gmail.com>
2022-01-03 21:07:52 +0600

    Added

--- Contest2B.cpp
+++ Contest2B.cpp
@@ -1,41 +1,24 @@
 #include<bits/stdc++.h>
 using namespace std;
+const string subseqs[]={"00","25","50","75"};
+const int INF =100;
+
+int solve(string s, string t)
+{
+   return s;
+}
+
 int main()
 {
-    int test;
-    //int zero=0, five=0;
-    string a;
-    cin>>test;
-    while(test--){
-     cin>>a;
-     int zero=0,z=0, five=0,f=0;
-     for(int i=a.size()-1;i>=0;i--)
-     {
-         if(a[i]=='0')
-         {
-           if(zero==1)
-           {
-               zero=2;
-               z=i;
-           }
-           if(zero==0){
-           zero=1;
-           }
-         }
-         if(a[i]=='5'){
-            five=1;
-            if(zero==1 && zero!=2){
-                zero=2;
-                z=i;
-            }
-         }
-         if(a[i]=='2' || a[i]=='7' && five==1 && five!=2){
-            five=2;
-            f=i;
-         }
-     }
-     cout<<z<<" Zero "<<zero<<endl;
-     cout<<f<<" Five "<<five<<endl;
+    int t=0;
+    cin>>t;
+    while(t--){
+        string n;
+        cin>>n;
+        int ans = INF;
+        for(auto e: subseqs)
+            ans = min(ans, solve(n,e));
 
+        cout<< ans<<endl;
     }
 }
