562a54e687a401575d428db51f8c52fe0b926105
gourab98 <gourabsahag@gmail.com>
2022-02-02 17:56:01 +0600

    Fibonacci

--- Test.cpp
+++ Test.cpp
@@ -1,39 +1,33 @@
+// Tree traversal in BFS in C++
+
 #include<bits/stdc++.h>
 using namespace std;
 int main()
 {
-    string s;
-    string p;
-    cin>>s;
-    cin>>p;
-    vector <int> g;
-    int arr[26] = {0};
-    int temp[26] = {0};
-
-    int slength = s.length();
-    int plength = p.length();
-    if(slength < plength)
+    int n;
+    cout<<"Enter the number of nodes: ";
+    cin>>n;
+    int arr[n];
+    cout<<"Enter the elements: ";
+    for(int i=0;i<n;i++)
     {
-        cout<<"No anagrams"<<endl;
-        return 0;
+        cin>>arr[i];
     }
-    //First Window
-    for(int i=0;i<plength;i++){
-        arr[p[i]-'a']++;
-        temp[s[i]-'a']++;
+    int root = arr[0];
+    queue<int> q;
+    q.push(root);
+    while(!q.empty())
+    {
+        int temp = q.front();
+        cout<<temp<<" ";
+        q.pop();
+        for(int i=0;i<n;i++)
+        {
+            if(arr[i]==temp)
+            {
+                q.push(arr[i]);
+            }
+        }
     }
-    if(arr==temp)
-        g.push_back(0);
-
-    //Rest of the windows
-    for(int i=plength;i<slength;i++){
-        temp[s[i-plength]-'a']--;
-        temp[s[i]-'a']++;
-
-        if(arr==temp)
-            g.push_back(i-plength+1);
-    }   
-    for(int i=0;i<g.size();i++)
-        cout<<g[i]<<" ";
-
+    
 }
