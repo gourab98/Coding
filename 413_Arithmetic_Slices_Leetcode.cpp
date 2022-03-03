565f76f243e6d1e94310adaf1ca391950517ba20
gourab98 <gourabsahag@gmail.com>
2022-03-03 13:12:25 +0600

    413 Arithmetic Slices Leetcode Problems

--- 413_Arithmetic_Slices_Leetcode.cpp
+++ 413_Arithmetic_Slices_Leetcode.cpp
@@ -20,3 +20,25 @@ public:
         return count;
     }
 };
+
+// Different Approach
+class Solution {
+public:
+    int numberOfArithmeticSlices(vector<int>& nums) {
+        int count=0;
+        int z=0;
+        int temp=0;
+        int p=0;
+        if(nums.size()<=2) return count;
+        for(int i=0;i<nums.size()-2;i++){
+              temp  = nums[i+1]-nums[i];
+              z = 0;
+           for(int j=i+2;j<nums.size();j++){
+               p = nums[j] - nums[j-1];
+               if(temp!=p) break;
+               else count++;
+           }
+        }
+        return count;
+    }
+};
