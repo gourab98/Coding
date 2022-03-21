ab47356ec08e5e9f17eb9ce566573b9ba5df442b
gourab98 <gourabsahag@gmail.com>
2022-03-21 10:53:55 +0600

    328 M Leetcode Problems

--- Test.cpp
+++ Test.cpp
@@ -1,12 +1,26 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int findout(){
-    
-}
-
 int main()
 {
-   
+    vector<int> vec;
+    vector<int> ans;
+    int input;
+    while(cin>>input){
+          vec.push_back(input);
+    }   
+    for(int i=0;i<vec.size();i++){
+        if(i%2==0){
+            ans.push_back(vec[i]);
+        }
+    }
+    for(int i=0;i<vec.size();i++){
+        if(i%2==1){
+            ans.push_back(vec[i]);
+        }
+    }
+    for(int i=0;i<ans.size();i++){
+        cout<<ans[i]<<" ";
+    }
   
 }  
