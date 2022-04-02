fb113fec6569db8f4395b8f9db7b9054315bdfa4
gourab98 <gourabsahag@gmail.com>
2022-04-02 11:18:56 +0600

    680 E Leetcode Problem

--- 680_E_Leetcode.cpp
+++ 680_E_Leetcode.cpp
@@ -1,28 +1,25 @@
-#include<bits/stdc++.h>
-using namespace std;
-
-int main()
-{
-    string s="abbca";
-     int j=s.size()-1;
+class Solution {
+public:
+    bool validPalindrome(string s) {
+        int j=s.size()-1;
         int i=0;
         for(i=0;i<s.size();i++,j--){
            if(s[i]!=s[j]){
                break;
            }
             }
-        vector<char> vec1;
-        vector<char> vec2;
+        vector<int> vec1;
+        vector<int> vec2;
         for(int k=0;k<s.size();k++){
             if(k!=i){
             vec1.push_back(s[k]);
             vec2.push_back(s[k]);    
             }
-           }
+}
         reverse(vec1.begin(),vec1.end());
-        if(vec1==vec2) cout<<"OKAY";
-
-       vec1.clear();
+        if(vec1==vec2) return true;
+        
+               vec1.clear();
        vec2.clear();
         
         for(int k=0;k<s.size();k++){
@@ -32,16 +29,9 @@ int main()
             }
 }
         reverse(vec1.begin(),vec1.end());
-        if(vec1==vec2) cout<<"Okay";
+        if(vec1==vec2) return true;
 
-        
-        for(int z=0;z<vec1.size();z++){
-            cout<<vec1[z]<<" ";
-        }
-
-        cout<<"NOT OKAY";
-
-    
-}  
-
-
+        return false;
+     
+    }
+};
