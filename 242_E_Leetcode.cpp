9f050d8b1c4f2fb84718ec9b073994e5263f5154
gourab98 <gourabsahag@gmail.com>
2022-04-03 19:01:10 +0600

    242 E (Another Solution) Leetcode Problem

--- 242_E_Leetcode.cpp
+++ 242_E_Leetcode.cpp
@@ -36,3 +36,19 @@ public:
         return true;
     }
 };
+
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        
+        if (s.length() != t.length())
+            return false;
+        
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        
+        if(s==t) return true;
+        else return false;
+    }
+};
