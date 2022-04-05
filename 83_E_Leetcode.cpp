fc0c3f2b44d88f36f841a6d24e837164e9a1860b
gourab98 <gourabsahag@gmail.com>
2022-04-05 21:43:18 +0600

    83 E Leetcode Problem

---
+++ 83_E_Leetcode.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        if(head==NULL || head->next == NULL) return head;
+        ListNode* ans = new ListNode(0);
+        ListNode* curr = ans;
+        ListNode* p = head;
+        while(p!=NULL && p->next!=NULL){
+            if(p->val == p->next->val){
+                p=p->next;
+            }
+            else{
+                curr->next = new ListNode(p->val);
+                curr = curr-> next;
+                p=p->next;
+            }
+        }
+        if(curr->val!=p->val){
+            curr->next = new ListNode(p->val);
+        }
+        
+        return ans->next;
+    }
+};
