541cea27e0335cda900b16640cd5c20d442577df
gourab98 <gourabsahag@gmail.com>
2022-08-31 21:34:44 +0600

    Test

--- Test.cpp
+++ Test.cpp
@@ -6,16 +6,16 @@ int main()
     ios_base::sync_with_stdio(false);
     cin.tie(0), cout.tie(0);
 
-    vector<int> time = {60,60,60, 120};
-         int count = 0 ;
-        vector<int>v(60,0);
-        for(int i=0;i<time.size();i++){
-            int a= time[i]%60;
-            if(a==0)
-                count = count + v[0];
-            else count = count + v[60-a];
-            
-            v[a]++;
+    vector<vector<int>> mat =  {{1,2,3},{4,5,6},{7,8,9}};
+    int m = mat.size();
+    reverse(mat.begin(),mat.end());
+    for(int x=0;x<mat.size();x++)
+    {
+       for (int y=0;y<mat[x].size();y++)
+       {
+           cout<<mat[x][y]<<" ";
+       }
+       cout<<endl;
+    }
 
-    cout << count << endl;
 }
