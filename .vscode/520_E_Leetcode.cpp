e72bc885f854dcb498fce93b76b5b23bb37cec14
gourab98 <gourabsahag@gmail.com>
2023-01-02 13:33:39 +0600

    Modified 520 E Leetcode

--- .vscode/520_E_Leetcode.cpp
+++ .vscode/520_E_Leetcode.cpp
@@ -1,22 +1,33 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int main(){
+int main()
+{
     string s;
-    cin>>s;
-    string s1,s2,s3;
-    s1 = s;
-    s2 = s;
-    s3 = s;
-    transform(s1.begin(),s1.end(), s1.begin(),:: toupper);
-    transform(s2.begin(),s2.end(), s2.begin(),:: tolower);
-    transform(s3.begin(),s3.end(), s3.begin(),:: tolower);
-    s3[0]= s3[0] - 32;
-    
-    if( s==s1 || s==s2 || s==s3){
-        cout<<"True";
+    cin >> s;
+    int count = 0;
+    if (s.size() == 1)
+    {
+        return true;
     }
-    else{
-        cout<<"False";
+    for (int i = 0; i < s.size(); i++)
+    {
+        if (isupper(s[i]))
+        {
+            count++;
+        }
+    }
+
+    if (count == 0 || count == s.size())
+    {
+        cout<<"True"<<endl;
+    }
+    else if (count == 1 && isupper(s[0]))
+    {
+        cout<<"True"<<endl;
+    }
+    else
+    {
+        cout<<"False"<<endl;
     }
     return 0;
 }
