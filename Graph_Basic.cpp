e972f81a7b0e76a4afbff18ecb3fbed1cc17bd3c
gourab98 <gourabsahag@gmail.com>
2023-06-04 10:31:36 +0600

    Added Graph Basic

---
+++ Graph_Basic.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void addEdge(vector<int> adj[],int u, int v){
+    adj[u].push_back(v);
+    adj[v].push_back(u);
+}
+
+void printGraph(vector<int> adj[], int v){
+    for(int i=0;i<v;i++){
+            cout<<i<<"-> ";
+        for(auto x: adj[i]){
+            cout<<x<<" ";
+        }
+        cout<<endl;
+    }
+}
+
+int main(){
+    int node = 4;
+    vector<int> adj[node];
+    addEdge(adj,0,1);
+    addEdge(adj,0,2);
+    addEdge(adj,1,2);
+    addEdge(adj,2,3); 
+
+    printGraph(adj,node);
+}
