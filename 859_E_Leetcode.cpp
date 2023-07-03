c42c52755d6e82970b6cb349cc9ba19c12d71da5
gourab98 <gourabsahag@gmail.com>
2023-07-03 10:45:51 +0600

    859 E Leetcode

---
+++ 859_E_Leetcode.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool buddyStrings(string s, string goal) {
+        if(s.size()!=goal.size() || s.size()==1) return false;
+        int a = 1;
+        for(int i=0;i<goal.size();i++){
+            if(s[i]!=goal[i]){
+                a=i;
+                break;
+            }
+        }
+        if(a==1){
+        for(int i=0;i<s.size()-1;i++){
+            if(s[i]==s[i+1]){
+                a=i+1;
+                break;
+            }
+        }
+        }
+            for(int j=0;j<s.size();j++){
+                if(a!=j){
+                    swap(s[a],s[j]);
+                    if(s==goal) return true;
+                    swap(s[j],s[a]);
+                }
+            }
+        return false;
+    }
+};
