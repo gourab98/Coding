a01d5285f87c8949a6b45dbbaba65b2a9b2cd7f5
gourab98 <gourabsahag@gmail.com>
2023-07-21 19:12:19 +0600

    673 M Leetcode

---
+++ 673_M_Leetcode.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int findNumberOfLIS(vector<int>& nums) {
+        int n= nums.size();
+        
+        vector<int> t(n,1);
+        vector<int> count(n,1);
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i;j++){
+                if(nums[j]<nums[i]){
+                    if(t[j]+1==t[i]){
+                        count[i]+=count[j];
+                    }
+                    else if(t[j]+1>t[i]){
+                        t[i] = t[j]+1;
+                        count[i]=count[j];
+                    }
+                }
+            }
+        }
+        
+        int maxval = *max_element(begin(t), end(t));
+        int result=0;
+        for(int i=0;i<n;i++){
+            if(t[i]==maxval){
+                result+=count[i];
+            }
+        }
+        return result;
+    }
+};
