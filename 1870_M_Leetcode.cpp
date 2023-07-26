6bb49e35b87f96becb445c5f193c85921085fd92
gourab98 <gourabsahag@gmail.com>
2023-07-26 18:39:05 +0600

    1870 M

---
+++ 1870_M_Leetcode.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    double possible(vector<int>& dist, int speed){
+        double time = 0.0;
+        int n =dist.size();
+        for(int i=0;i<n-1;i++){
+            double t = (double)dist[i]/(double)speed;
+            time +=ceil(t);
+        }
+        time +=(double)dist[n-1]/(double)speed;
+        return time;        
+    }
+    
+    int minSpeedOnTime(vector<int>& dist, double hour) {
+        int l=1;
+        int r = 1e7;
+        int result = -1;
+        while(l<=r){
+            int mid = l+(r-l)/2;
+            if(possible(dist,mid)<=hour){
+                result = mid;
+                r = mid-1;
+            }
+            else {
+                l = mid+1;
+            }
+        }
+
+        return result;
+        
+    }
+};
