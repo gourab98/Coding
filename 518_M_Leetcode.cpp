e10dad27ec85c23bf8a796edf1b76490bb7bb89f
gourab98 <gourabsahag@gmail.com>
2023-08-11 12:25:35 +0600

    518 M Leetcode

---
+++ 518_M_Leetcode.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int t[301][5001];
+    int solve(int start, int amount, vector<int>& coins){
+        if(amount==0){
+            return 1;
+        }
+        if(start==coins.size() || amount<0){
+            return 0;
+        }
+        if(t[start][amount]!=-1){
+            return t[start][amount];
+        }
+        int take = solve(start,amount-coins[start],coins);
+        int skip = solve(start+1,amount,coins);
+        
+        return t[start][amount]=take+skip;
+        
+    }
+    
+    int change(int amount, vector<int>& coins) {
+        memset(t,-1,sizeof(t));
+        return solve(0,amount,coins);
+    }
+};
