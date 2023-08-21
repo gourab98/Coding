59483e2a36aa547efa071cc2de58b2358147a867
gourab98 <gourabsahag@gmail.com>
2023-08-21 22:20:54 +0600

    updated

--- 459_E_Leetcode.cpp
+++ 459_E_Leetcode.cpp
@@ -3,18 +3,16 @@ public:
     bool repeatedSubstringPattern(string s) {
         
         int n=s.length();
-        for(int i=1;i<=n/2;i++){
-            if(n%i == 0){
-                int times = n/i;
-                string pattern = s.substr(0,i);
-                string newstr="";
+        for(int i=n/2;i>=1;i--){
+            if(n%i==0){
+                int time = n/i;
+                string str = s.substr(0,i);
+                string newStr = "";
+                while(time--){
+                    newStr+=str;
+                }
                 
-                while(times--){
-                    newstr+=pattern;
-                }
-                if(newstr==s){
-                    return true;
-                }
+                if(newStr==s) return true;
             }
         }
         return false;
